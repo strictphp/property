e830e229fdd8d09257b4f7ca8deb5f84252e289f
4kizuki <akizuki.c10.l65@gmail.com>
2017-12-05 12:37:47 +0900

    Add basic classes with traits

---
+++ src/Utility/ClassWithDisablePropertyInjection.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Strict\Property\Utility;
+
+use Strict\Property\DisablePropertyInjection;
+
+
+/**
+ * [ Base ] Class with DisablePropertyInjection
+ *
+ * @author 4kizuki <akizuki.c10.l65@gmail.com>
+ * @copyright 2017 4kizuki. All Rights Reserved.
+ * @package strictphp/property
+ * @since 1.2.0
+ */
+class ClassWithDisablePropertyInjection
+{
+
+    use DisablePropertyInjection;
+
+}
---
+++ src/Utility/ClassWithStandardPropertyAccess.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Strict\Property\Utility;
+
+use Strict\Property\StandardPropertyAccess;
+
+
+/**
+ * [ Base ] Class with StandardPropertyAccess
+ *
+ * @author 4kizuki <akizuki.c10.l65@gmail.com>
+ * @copyright 2017 4kizuki. All Rights Reserved.
+ * @package strictphp/property
+ * @since 1.2.0
+ */
+class ClassWithStandardPropertyAccess
+{
+
+    use StandardPropertyAccess;
+
+}
